fa28ac5c39d230407693dfea85be2b16e51909b4
bsuth <bsuth701@gmail.com>
2021-09-25 22:19:39 +0900

    add src

--- main.lua
+++ main.lua
@@ -1,3 +1,11 @@
+package.path = package.path .. ';./src/?.lua'
+local utils = require('utils')
+
+function love.load() end
+
+function love.update(dt) end
+
 function love.draw()
+  utils.setHex('#ff0000')
   love.graphics.print('Hello World', 400, 300)
 end
